c4d4fbb98ab2eb13bb003d8e610e090752c800b3
thiago <trcosta97@hotmail.com>
2026-02-16 17:57:37 -0300

    start repo

---
+++ instalar.bat
@@ -0,0 +1,6 @@
+@echo off
+echo Instalando dependencias...
+python -m pip install pandas numpy matplotlib seaborn scikit-learn openpyxl jupyter
+echo.
+echo Concluido! Execute: jupyter notebook
+pause
